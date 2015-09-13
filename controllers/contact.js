b6326810653a5257fcf33a7f6b9b462d8c138b26
Naeem Talukdar <naeem.talukdar@gmail.com>
2015-09-13 09:10:21 -0400

    Implementing Mandrill

--- controllers/contact.js
+++ controllers/contact.js
@@ -37,7 +37,7 @@ exports.postContact = function(req, res) {
   var from = req.body.email;
   var name = req.body.name;
   var body = req.body.message;
-  var to = 'your@email.com';
+  var to = 'naeem.talukdar@gmail.com';
   var subject = 'Contact Form | Hackathon Starter';
 
   var mailOptions = {
