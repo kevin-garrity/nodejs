0eab46c74f3d78f98d95133106652b5579caed1c
Naeem Talukdar <naeem.talukdar@gmail.com>
2015-11-04 11:23:06 -0500

    launchrock shenanigans

--- app.js
+++ app.js
@@ -94,6 +94,25 @@ app.use(function(req, res, next) {
   console.log(res.locals.user);
   next();
 });
+// Add headers
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 app.use(function(req,res,next){
   if(res.locals.user){
     Application.find({ $or: [ {coach: res.locals.user.customid, status: 'pending', coachRead: false }, {athlete: res.locals.user.customid, status: { $ne: 'pending' }, athleteRead: false } ]}, function(err, applications) {
